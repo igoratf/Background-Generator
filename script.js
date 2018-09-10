da86de584701a3914f2bc90d6332a0784391aa84
igoratf <igor.atf@gmail.com>
2018-09-10 20:39:08 -0300

    set default background

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.querySelector("body");
+var body = document.getElementById("bg-gradient");
 
+
+(function setDefaultBackground() {
+    body.style.background = "linear-gradient(to right, #ff0000, #ffff00)";
+    css.textContent = body.style.background + ";";
+})()
 
 color1.addEventListener("input", changeBackground);
 color2.addEventListener("input", changeBackground);
